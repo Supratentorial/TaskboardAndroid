bcbad7eb28a81fe714c2f06533d7dd3cafb2d379
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-05-11 00:02:34 +1000

    Initial commit.

---
+++ app/src/main/java/org/monash/taskboardandroid/models/Task.java
@@ -0,0 +1,17 @@
+package org.monash.taskboardandroid.models;
+
+/**
+ * Created by blake on 10/05/2016.
+ */
+public class Task {
+
+    private int taskId;
+
+    public int getTaskId(){
+        return  this.taskId;
+    }
+
+    public void setTaskId(int taskId){
+         this.taskId = taskId;
+    }
+}
