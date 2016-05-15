1a8df48a6cbd2fde5e6d9c4488f114ee4aa9928d
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-05-16 07:53:04 +1000

    Fixed fragment error.

--- app/src/main/java/org/monash/taskboardandroid/fragments/TaskDetailFragment.java
+++ app/src/main/java/org/monash/taskboardandroid/fragments/TaskDetailFragment.java
@@ -1,6 +1,9 @@
 package org.monash.taskboardandroid.fragments;
 
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,6 +13,8 @@ import android.widget.TextView;
 
 import org.monash.taskboardandroid.AppConstants;
 import org.monash.taskboardandroid.R;
+import org.monash.taskboardandroid.TaskContentProvider;
+import org.monash.taskboardandroid.datalayer.DatabaseContract;
 
 
 public class TaskDetailFragment extends Fragment {
@@ -23,8 +28,7 @@ public class TaskDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Bundle bundle = this.getArguments();
-        Long patientId = bundle.getLong(AppConstants.PatientId);
+
     }
 
     @Override
@@ -32,8 +36,17 @@ public class TaskDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_task_detail, container, false);
         TextView patientNameTextView = (TextView)view.findViewById(R.id.patient_name);
-
-
+        Bundle bundle = this.getArguments();
+        Long patientId = bundle.getLong(AppConstants.PatientId);
+        ContentResolver contentResolver = this.getActivity().getContentResolver();
+        String selectionClause = DatabaseContract.COLUMN_NAME_PATIENT_ID + "=?";
+        String[] selectionArgs = { patientId.toString() };
+        Cursor cursor = contentResolver.query(TaskContentProvider.CONTENT_URI_TASKS,null, selectionClause, selectionArgs, null);
+        Log.d(TAG, DatabaseUtils.dumpCursorToString(cursor));
+        if(cursor!= null) {
+            cursor.moveToFirst();
+            patientNameTextView.setText(cursor.getString(cursor.getColumnIndex(DatabaseContract.COLUMN_NAME_PATIENT_LAST_NAME)));
+        }
         return view;
     }
 
